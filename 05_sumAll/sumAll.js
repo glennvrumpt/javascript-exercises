a34eeff13fc8258ba06f05103d79da2c3628160d
Glenn van Rumpt <glennvrumpt@outlook.com>
2022-08-11 16:48:43 +0200

    Add 05 solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+  var sum = 0;
+  if (
+    num1 < 0 ||
+    num2 < 0 ||
+    !Number.isFinite(num1) ||
+    !Number.isFinite(num2)
+  ) {
+    return "ERROR";
+  } else {
+    for (var i = Math.min(num1, num2); i <= Math.max(num1, num2); i++) {
+      sum += i;
+    }
+  }
+  return sum;
 };
 
 // Do not edit below this line
