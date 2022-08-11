4c3ad4797278490f1c55ddc5b02e32fdd3a3c8de
Glenn van Rumpt <glennvrumpt@outlook.com>
2022-08-11 17:40:09 +0200

    Add solution 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function (num) {
+  temp = (num - 32) * 0.5556;
+  return parseFloat(temp.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function (num) {
+  temp = num * 1.8 + 32;
+  return parseFloat(temp.toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
